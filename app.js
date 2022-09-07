21f996a34e2269e866894ed3b49611111593abfc
Stivennevits <stivennevits16@gmail.com>
2022-09-07 12:44:12 -0500

    otro console.log

---
+++ app.js
@@ -0,0 +1,31 @@
+let archivoMovies = require("./movies")
+
+let moviesDH = {
+    movies: archivoMovies,
+      listMovies: function(){
+        this.movies.forEach((pelicula,i) => {
+            console.log((i + 1) + ". " + pelicula.title)
+        })
+    },
+      searchMovies: function(parametro){
+        let found = this.movies.find(peliculas => peliculas.id == parametro || peliculas.title == parametro);
+        found == undefined ? console.log("Lo sentimos, no está disponible.") : console.log(found)
+
+    },
+      searchMoviesByGenre: function(genero){
+        let foundGenre = this.movies.filter(peliculas => peliculas.genre == genero)
+
+        foundGenre.length == 0 ? console.log("No tenemos de esas ") : console.log(foundGenre)
+
+      }
+    
+}
+moviesDH.listMovies()
+console.log("🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈")
+moviesDH.searchMovies(2)
+moviesDH.searchMovies("Coco")    
+console.log("🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈")
+moviesDH.searchMoviesByGenre("Suspesnso")
+moviesDH.searchMoviesByGenre("acción")
+console.log("🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈")
+console.log("🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈🎈")
---
+++ movies.js
@@ -0,0 +1,62 @@
+const movies = [
+    {
+        id: 1,
+        title: "El conjuro",
+        rating: 9.0,
+        awards: 31,
+        length: 162,
+        price: 890,
+        genre: "Suspenso"
+    },
+
+    {
+        id: 2,
+        title: "Busqueda implacable",
+        rating: 8.6,
+        awards: 25,
+        length: 132,
+        price: 630,
+        genre: "acción"
+    },
+
+    {
+        id: 3,
+        title: "Coco",
+        rating: 9.3,
+        awards: 34,
+        length: 126,
+        price: 880,
+        genre: "animación"
+    },
+
+    {
+        id: 4,
+        title: "Resident evil v",
+        rating: 8.0,
+        awards: 36,
+        length: 144,
+        price: 920,
+        genre: "acción"
+    },
+
+    {
+        id: 5,
+        title: "El origen",
+        rating: 8.9,
+        awards: 36,
+        length: 162,
+        price: 740,
+        genre: "acción"
+    },
+    {
+        id: 6,
+        title: "Inferno",
+        rating: 7.9,
+        awards: 31,
+        length: 142,
+        price: 540,
+        genre: "suspenso"
+    }
+]
+
+module.exports = movies;
